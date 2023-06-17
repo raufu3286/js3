a57c45c8f0cfcf5a83382aad0c0c7d7d2fa54e47
eshikhon-mern-stack <mern2.eshikhon@gmail.com>
2023-06-18 01:12:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,231 @@
+//if else statement
+ var number = 50;
+ if (number >= 90){
+  //  console.log("Grade= A");
+ }
+ else if (number<90 && number>=87){
+  //  console.log("Grade= B+");
+ }
+ else if (number<87 && number>=85){
+  //  console.log("Grade= B");
+ }
+ else if (number<85 && number>=81){
+  //  console.log("Grade= B-");
+ }
+ else if (number<81 && number>=75){
+  //  console.log("Grade= C+");
+ }
+ else if (number<75 && number>=70){
+  //  console.log("Grade= C");
+ }
+ else if (number<70 && number>=65){
+  //  console.log("Grade= D+");
+ }
+ else if (number<65 && number>=60){
+  //  console.log("Grade= B");
+ }
+ else{
+   // console.log("Grade= F")
+ }
+
+
+ //check character
+
+ var character = "$" ;
+ if ((character>='a'&& character<='z') || (character>='A'&& character<='B')){
+  //  console.log("Alphabet");
+ }
+ else if (character>='0'&& character<='9')
+ {
+   // console.log("Number");
+ }
+ else{
+   //  console.log("Special character");
+ }
+
+ //for loop
+ //var num=1;
+ for(num=0;num<=10;num++)
+ {
+   // console.log(num);
+ }
+ var num=50;
+ for(var i=1;i<=num;i+=2){
+  //  console.log(i);
+ }
+
+
+
+ //sum of numbers
+ var num=10;
+ var sum=0;
+ for(var i=1;i<=num;i++){
+     sum+=i;
+  //   console.log(sum);
+ }
+ //  console.log(" Total Sum=",sum);
+  
+
+
+
+ //Multiplication table
+
+ var num=5;
+ for(var i=1;i<=10;i++)
+ {
+  //  console.log(num,"*",i,"=",num*i);
+ }
+
+
+
+var a=[10,20,30,60,70,90,100];
+for(var i=0;i<a.length;i++)
+{
+    var element=a[i];
+   // console.log(element);
+}
+
+
+//sum
+var B=[10,11,15,18,20,30,40,50];
+var sum1=0;
+for(var i=0;i<=a.length;i++)
+{
+    var element=B[i];
+   // console.log(element);
+    sum1+=B[i];
+}
+// console.log(sum1);
+
+var c=[12,14,16,17,18,20,25,26,28];
+
+for(var i=0;i<c.length;i+=2)
+{
+    var element=c[i];
+   // console.log(element);
+}
+
+
+
+
+
+
+
+
+//while loop
+
+var num=1;
+ while(num<=10)
+ {
+  // console.log(num);
+   num++;
+ }
+ var num=50;
+ var i=1;
+ while(i<=num){
+    //console.log(i);
+  i+=2;
+ }
+
+
+
+ //sum of numbers
+ var num=10;
+ var sum=0;
+ var i=1;
+ while(i<=num){
+     sum+=i;
+  //   console.log(sum);
+  i++;
+ }
+ //  console.log(" Total Sum=",sum);
+  
+
+
+
+ //Multiplication table
+
+ var num=5;
+ var i=1;
+ while(i<=10)
+ {
+  //  console.log(num,"*",i,"=",num*i);
+  i++;
+ }
+
+
+
+var a=[10,20,30,60,70,90,100];
+var i=0;
+while(i<a.length)
+{
+    var element=a[i];
+  // console.log(element);
+   i++;
+}
+
+
+//sum
+var B=[10,11,15,18,20,30,40,50];
+var sum1=0;
+var i=0;
+while(i<=a.length)
+{
+    var element=B[i];
+   // console.log(element);
+    sum1+=B[i];
+    i++;
+}
+// console.log(sum1);
+
+var c=[12,14,16,17,18,20,25,26,28];
+var i=0;
+
+for(i<c.length)
+{
+    var element=c[i];
+   // console.log(element);
+   i+=2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
